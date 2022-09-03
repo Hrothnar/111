6c6e7835c8da10bdc23b13fe7de85e23e47181f8
Kirill <hrothnar@mail.ru>
2022-09-03 18:30:26 +0300

    Add another exercise

--- src/main.java
+++ src/main.java
@@ -7,5 +7,23 @@ public class main {
 	} else {
 		System.out.println("Средний вес выше допустимого");
 	}
+	
+	
+		//task1
+		int cube = 7; // 1-6
+		switch (cube) {
+			case 2:
+			case 4:
+			case 6:
+				System.out.println("У тебя чётное число");
+				break;
+			case 1:
+			case 3:
+			case 5:
+				System.out.println("У тебя нечётное число");
+				break;
+			default:
+				System.out.println("Хм... да ты везунчик!");
+		}
 	}
 }
