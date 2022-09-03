bf3976d2e1abf1ec82784dda7c5811dcadcb0f99
Kirill <hrothnar@mail.ru>
2022-09-03 18:16:31 +0300

    init commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+	public static void main(String[] args) {
+	int averageWeight = 32;
+	if (averageWeight < 30) {
+		System.out.println("Средний вес ниже допустимого");
+	} else {
+		System.out.println("Средний вес выше допустимого");
+	}
+	}
+}
